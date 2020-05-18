8a7ccd0a734dfcfe452a7e786138ec13f42903b8
RyanAsh93 <blueheadryan93@yahoo.com>
2020-05-18 14:36:26 -0600

    update working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,6 +26,20 @@ class PagesController < ApplicationController
     #params[:page] gives me the form fields
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if (@page.update(pages_params))
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def pages_params
